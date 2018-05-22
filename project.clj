f86532e38110886fe29d820cf910799652a6a1ae
Andrey Fadeev <andrey.fadeev@flocktory.com>
2018-05-22 16:28:50 +0300

    deploy

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject com.flocktory.component/kafka-log-tracer "0.0.1"
   :description "Log tracer for kafka consumer"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/flocktory/kafka-log-tracer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.flocktory/kafka-consumer "0.0.5"]])
+  :dependencies [[com.flocktory/kafka-consumer "0.0.5"]]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :sign-releases false}]])
