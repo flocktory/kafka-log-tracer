8cd78a008ff340b30653664e6b4e381b68414a08
Andrew Rezcov <oxgrouby@gmail.com>
2020-10-20 17:19:07 +0300

    Up for https://github.com/flocktory/kafka-consumer/commit/781eb22dec1346be683ce93454acccf441d9bc50 (#1)

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.flocktory.component/kafka-log-tracer "0.0.1"
+(defproject com.flocktory.component/kafka-log-tracer "0.0.2"
   :description "Log tracer for kafka consumer"
   :url "https://github.com/flocktory/kafka-log-tracer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.flocktory/kafka-consumer "0.0.5"]]
+  :dependencies [[com.flocktory/kafka-consumer "0.0.9"]]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :sign-releases false}]])
